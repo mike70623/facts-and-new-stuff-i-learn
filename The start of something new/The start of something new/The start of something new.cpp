5c8c6217073f4db0d950a5e863ae24e095974cb6
game110pm <game110pm@605-110-STU07>
2017-01-30 13:51:17 -0700

    initial

---
+++ The start of something new/The start of something new/The start of something new.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main (){
+	char name;
+	cout << "this is my fist test game. please enter the fist letter of your name" << endl;
+	cin >> name;
+	cout << "welcome, " << name << endl;
+}
