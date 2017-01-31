3a0a05284dd8d107c18d3bdafaa13ece42c5d54f
game110pm <game110pm@605-110-STU07>
2017-01-31 13:45:34 -0700

    part 2 of something

---
+++ 1-20-17/1-20-17/rectangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int width;
+	int length;
+	int area;
+	cout << "what is the width?" << endl;
+	cin >> width;
+	cout << "what is the length?" << endl;
+	cin >> length;
+	area = width*length;
+	cout << "the area is " << area << endl;
+}
