4dc6a03a0e26370f817c093786239aa2f0b899aa
game110pm <game110pm@605-110-STU07>
2017-02-08 14:16:04 -0700

    story development-The past

---
+++ log 4/log 4/The start of something interesting.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int passcode;
+	int code;
+	string name;
+	cout << "identify yourself" << endl;
+	cin >> name;
+	cout << "welcome, " << name << ".enter passcode" << endl;
+	cin >> passcode;
+	if (passcode == 777)
+		cout << "entry log #1" << endl << "We recieved a request to research an interesting phenomenon today." << endl << "Apparently people all around have been experiencing a strange biological occurance in which they are able to move, make and destroy certain combinations of molecules. Some fire, some rock." << endl << "even stranger is that thoes that have had this expirence lose the need to eat or drink." << endl;
+	else
+		cout << "access denied" << endl << "terminating file" << endl;
+
+}
